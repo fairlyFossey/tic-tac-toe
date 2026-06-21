414aabd04f5c90ff8ac5f678546cafca45d7e353
fairlyFossey <188269976+fairlyFossey@users.noreply.github.com>
2026-06-21 02:16:10 -0400

    Create Game-board object

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
-    // gameboard object
-        // keep track of occupied squares using an array
-        // reset board state
+// gameboard object
+const gameboard = (() => {
+    let grid = [
+        undefined, undefined, undefined,
+        undefined, undefined, undefined,
+        undefined, undefined, undefined,
+    ];
 
-    // playerX and playerO objects
-        // mark a square as occupied
-            // check if square is empty
-            // modify value of relevant indice
-        // create a player name variable
-            // create a setter method for player name
-        // create a player score variable
-            // increment score method
+    function occupyTile(pos, player) {
+        if (grid[pos] == undefined) {
+            grid[pos] = player;
+            // call gameflow-endturn method
+        }
+    }
+    function getBoardState() {
+        return grid;
+    }
+    function resetBoardState() {
+        grid = grid.map((tile) => tile = undefined);
+    }
 
-    // gameflow object
-        // turn manager
-            // create a current-turn variable
-            // create an end-turn method that checks for win/stalemate and then toggles the current turn variable
-            // create a method to randomize the value of current turn variable to determine starting player
-        // start new game
-            // call the gameboard-reset method
-            // call current-turn-randomizer method
-            // call to update display
-        // update display
+    return { occupyTile, getBoardState, resetBoardState }
+})();
+
+// playerX and playerO objects
+    // create a player name variable
+        // create a setter method for player name
+    // create a player score variable
+        // increment score method
+
+// gameflow object
+    // turn manager
+        // create a current-turn variable
+        // create an end-turn method that checks for win/stalemate and then toggles the current turn variable
+        // create a method to randomize the value of current turn variable to determine starting player
+    // start new game
+        // call the gameboard-reset method
+        // call current-turn-randomizer method
+        // call to update display
+    // update display
 
 
     
