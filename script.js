cea60aa7b7f8f1f639498994a1c12e7d2926a61f
fairlyFossey <188269976+fairlyFossey@users.noreply.github.com>
2026-06-21 01:07:53 -0400

    Add pseudo-code

---
+++ script.js
@@ -0,0 +1,26 @@
+    // gameboard object
+        // keep track of occupied squares using an array
+        // reset board state
+
+    // playerX and playerO objects
+        // mark a square as occupied
+            // check if square is empty
+            // modify value of relevant indice
+        // create a player name variable
+            // create a setter method for player name
+        // create a player score variable
+            // increment score method
+
+    // gameflow object
+        // turn manager
+            // create a current-turn variable
+            // create an end-turn method that checks for win/stalemate and then toggles the current turn variable
+            // create a method to randomize the value of current turn variable to determine starting player
+        // start new game
+            // call the gameboard-reset method
+            // call current-turn-randomizer method
+            // call to update display
+        // update display
+
+
+    
